680ef251fe26f8a564c4d603dab9e1918af8df4a
CFieldhouse <timpat1111@gmail.com>
2021-04-04 22:15:20 +0100

    initial working basic widget

---
+++ app/src/main/java/com/example/clockcalendar/ClockActivity.kt
@@ -0,0 +1,73 @@
+package com.example.clockcalendar
+
+//import android.R
+import android.R.drawable
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.widget.RemoteViews
+import androidx.appcompat.app.AppCompatActivity
+import java.lang.reflect.Field
+import java.util.*
+
+
+class ClockActivity : AppWidgetProvider() {
+
+    fun getDrawableName(ctx: Context, str: String?): Int {
+        return ctx.resources.getIdentifier(str, "drawable", ctx.packageName)
+    }
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        Log.v("hi", "hello");
+        // Perform this loop procedure for each App Widget that belongs to this provider
+        appWidgetIds.forEach { appWidgetId ->
+            RemoteViews(context.packageName, R.layout.clock_layout).also { views->
+                val time: Date = Calendar.getInstance().time
+                val month = time.month
+                val day = time.date
+                val id_string = "ic_clock_hand_" + month + "_" + day
+                Log.v("id", id_string)
+                val myhand = getDrawableName(context, id_string)
+                //val myhand = getResId(id_string, drawable::class.java)
+                views.setImageViewResource(R.id.clock_hand, myhand)
+                appWidgetManager.updateAppWidget(appWidgetId, views)
+            }
+        }
+    }
+
+    override fun onAppWidgetOptionsChanged(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetId : Int,
+        newOptions: Bundle
+    ){
+        Log.d("hello", "from resize etc.")
+        RemoteViews(context.packageName, R.layout.clock_layout).also { views->
+            val time: Date = Calendar.getInstance().time
+            val month = time.month
+            val day = time.date
+            val id_string = "ic_clock_hand_" + month + "_" + day
+            Log.v("id", id_string)
+            val myhand = getDrawableName(context, id_string)
+            //val myhand = getResId(id_string, drawable::class.java)
+            views.setImageViewResource(R.id.clock_hand, myhand)
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+}
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.clock_layout)
+        Log.d("hello", "from resize etc.")
+    }
+}
+
---
+++ app/src/main/res/write_clock_hands.py
@@ -0,0 +1,27 @@
+import numpy as np
+def line(to):
+    return (
+        """
+        <vector xmlns:android="http://schemas.android.com/apk/res/android"
+            android:width="79.64dp"
+            android:height="79.64dp"
+            android:viewportWidth="79.64"
+            android:viewportHeight="79.64">
+          <path\n"""
+              + f"\t\t\tandroid:pathData=\"M40.0,40.0 L{40+to[0]},{40+to[1]}\"\n"
+              + """\t\t\tandroid:strokeLineJoin="miter"
+              android:strokeWidth="0.26458332"
+              android:fillColor="#00000000"
+              android:strokeColor="#00ffff"
+              android:strokeLineCap="butt"/>
+        </vector>
+        """
+    )
+
+for month in range(12):
+    for day in range(31):
+        with open(f"drawable/ic_clock_hand_{month}_{day}.xml", "w") as f:
+            f.write(line([
+                40 * np.sin((month * 31 + day) * 2 * np.pi / (12 * 31)),    
+                40 * np.cos((month * 31 + day) * 2 * np.pi / (12 * 31)),
+            ]))
